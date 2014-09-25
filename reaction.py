91f188ccf9db1288ec6f2ba207021b789ec9c360
shellymoney <acuatepo@uci.edu>
2014-09-24 23:50:55 -0700

    more testing

--- reaction.py
+++ reaction.py
@@ -9,10 +9,16 @@ led = 23
 
 GPIO.setup(led, GPIO.OUT)
 
+GPIO.output(led, 0)
+
 #pausing for a random time between 5 and 10 sec
 time.sleep(random.uniform(5, 10))
 
 GPIO.output(led, 1)
+
+time.sleep(2)
+
+GPIO.output(led, 0)
 
 GPIO.cleanup()
 #button = 
