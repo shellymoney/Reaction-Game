0ac618674dc19b0b4ef6148b2a97c2e9aac68191
shellymoney <acuatepo@uci.edu>
2014-09-24 23:34:22 -0700

    testing LED

--- reaction.py
+++ reaction.py
@@ -1,1 +1,14 @@
-print "poop"
+import RPi.GPIO as GPIO
+import time
+import random
+
+#default GPIO pin numbering
+GPIO.setmode(GPIO.BOARD)
+
+led = 23
+
+GPIO.setup(led, GPIO.OUT)
+
+time.sleep(random.uniform(5, 10))
+
+#button = 
