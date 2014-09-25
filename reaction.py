bb8367f127eb6f74df1273d74cc68a9bb8bbb94d
shellymoney <acuatepo@uci.edu>
2014-09-24 23:58:50 -0700

    fixed button

--- reaction.py
+++ reaction.py
@@ -12,11 +12,11 @@ GPIO.setup(led, GPIO.OUT)
 GPIO.setup(button, GPIO.IN)
 
 GPIO.output(led, 0)
-while GPIO.input(button)
+while GPIO.input(button):
 	pass
-if GPIO.input(button) == False
+if GPIO.input(button) == False:
 	print "Button pressed"
-	
+
 #pausing for a random time between 5 and 10 sec
 time.sleep(random.uniform(5, 10))
 
